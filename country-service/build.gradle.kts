3ea9a580aefeed30fe076c0a2609a163c1cfe1c1
dyusupov <dmitry.yusupov@infobip.com>
2020-10-28 18:11:09 +0300

    Fixed error

--- country-service/build.gradle.kts
+++ country-service/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 dependencies {
     implementation(project(":city-model"))
     implementation(project(":common"))
-    implementation(project(":starters"))
+    //implementation(project(":starters"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-aop")
--- country-service/src/main/kotlin/ru/yusdm/training/country/api/CountryController.kt
+++ country-service/src/main/kotlin/ru/yusdm/training/country/api/CountryController.kt
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.yusdm.training.common.logger
+import ru.yusdm.training.common.sleuth.SleuthBaggageFieldMaintainer.getBaggageField
+import ru.yusdm.training.common.sleuth.SleuthBaggageFieldMaintainer.setBaggageField
 import ru.yusdm.training.country.model.Country
 import ru.yusdm.training.country.service.CountryService
 import java.util.concurrent.atomic.AtomicInteger
@@ -22,8 +24,8 @@ class CountryController(private val countryService: CountryService) {
 
     @GetMapping(value = ["/{countryId}"])
     fun getCountryById(@PathVariable countryId: Long): ResponseEntity<Country> {
-//        setBaggageField(USER_ID, "TestUser ${counter.incrementAndGet()}")
-//        log.info("User id is " + getBaggageField(USER_ID))
+        setBaggageField(USER_ID, "TestUser ${counter.incrementAndGet()}")
+        log.info("User id is " + getBaggageField(USER_ID))
 
         return countryService.findByCountryId(countryId = countryId)?.let {
             ResponseEntity.ok(it)
